1f8a8bbe4874de975c82e221beca47c00a01c27d
Eduard <eduardcosma2000@gmail.com>
2021-10-18 17:28:38 +0300

    Refactored the naming of the functions

--- src/main.java
+++ src/main.java
@@ -4,13 +4,13 @@ public class main {
 
     public static void main(String[] args) {
         SymbolTable symbolTable = new SymbolTable();
-        System.out.println(symbolTable.getPosition("a"));
-        System.out.println(symbolTable.getPosition("a"));
-        System.out.println(symbolTable.getPosition("1"));
-        System.out.println(symbolTable.getPosition("2"));
-        System.out.println(symbolTable.getPosition("3"));
-        System.out.println(symbolTable.getPosition("3"));
-        System.out.println(symbolTable.getPosition("1"));
-        System.out.println(symbolTable.getPosition("a"));
+        System.out.println(symbolTable.addAndReturnPosition("a"));
+        System.out.println(symbolTable.addAndReturnPosition("a"));
+        System.out.println(symbolTable.addAndReturnPosition("1"));
+        System.out.println(symbolTable.addAndReturnPosition("2"));
+        System.out.println(symbolTable.addAndReturnPosition("3"));
+        System.out.println(symbolTable.addAndReturnPosition("3"));
+        System.out.println(symbolTable.addAndReturnPosition("1"));
+        System.out.println(symbolTable.addAndReturnPosition("a"));
     }
 }
--- src/model/SymbolTable.java
+++ src/model/SymbolTable.java
@@ -16,7 +16,7 @@ public class SymbolTable {
      * @param key which is the token
      * @return position of the token
      */
-    public int getPosition(String key){
+    public int addAndReturnPosition(String key){
         int position = search(key);
         if (position == -1)
         {
@@ -32,7 +32,7 @@ public class SymbolTable {
      * @param key the token we want to add
      * @param value the position of the token
      */
-    public void add(String key, int value) {
+    private void add(String key, int value) {
         Node nodeToAdd = new Node(key,value);
 
         if (root == null) {
@@ -64,7 +64,7 @@ public class SymbolTable {
      * @param key the token for which we are looking for
      * @return the position of the token
      */
-    public int search(String key) {
+    private int search(String key) {
         Node currentNode = root;
         while (currentNode != null){
             if(currentNode.key.compareTo(key) > 0){
@@ -86,7 +86,7 @@ public class SymbolTable {
      * @param currentNode starting node from where we want to start the inorder traversal
      * @param queue an empty queue for storing the order in which the nodes were visited
      */
-    public void inorderTraversal(Node currentNode, Queue<Node> queue){
+    private void inorderTraversal(Node currentNode, Queue<Node> queue){
         if (currentNode == null)
             return;
         inorderTraversal(currentNode.leftChild,queue);
@@ -97,10 +97,9 @@ public class SymbolTable {
     /**
      * This method reorders the tokens alphabetically
      */
-    public void nodesInAsscendingOrder() {
+    private void nodesInAsscendingOrder() {
         Queue<Node> queue = new LinkedList<Node>();
         inorderTraversal(this.root, queue);
-        System.out.println(queue.toString());
         int index = 0;
         while (!queue.isEmpty()) {
             Node currentNode = queue.remove();
